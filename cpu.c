021262f9441dae36d493db1a03853152568db1df
Reversal312 <senbala312@email.com>
2026-09-24 23:04:23 -0500

    finished cpu reading of the instruction code

--- cpu.c
+++ cpu.c
@@ -8,4 +8,17 @@ void cpu_init(cpu_t *c)
   memset(c, 0, sizeof(cpu_t));
 }
 
+void step(cpu_t *c)
+{
+  uint32_t by0 = c->mem[c->pc];
+  uint32_t by1 = c->mem[c->pc+1];
+  uint32_t by2 = c->mem[c->pc+2];
+  uint32_t by3 = c->mem[c->pc+3];
+
+  uint32_t instruction = (by0 << 24) | (by1 << 16) | (by2 << 8) | by3;
+
+  printf("pc=%08x instruction=%08x\n", c->pc, instruction);
+
+  c->pc += 4;
+}
 
